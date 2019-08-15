8578ed2c011113fbec76693fbdab3a7e82905d7d
Simanta <syed.simanta06@gmail.com>
2019-08-16 00:15:53 +0600

    Updates

---
+++ precache-manifest.8fcc6b5f6050c5365e11c0a8c3f09520.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80c278f605ab0f634f34d020dde1b69",
+    "url": "/reestora-react-cart/index.html"
+  },
+  {
+    "revision": "a1ca7312802aaf110ca7",
+    "url": "/reestora-react-cart/static/css/main.d3960174.chunk.css"
+  },
+  {
+    "revision": "34bc87ac89ea319d3878",
+    "url": "/reestora-react-cart/static/js/2.8f58997b.chunk.js"
+  },
+  {
+    "revision": "a1ca7312802aaf110ca7",
+    "url": "/reestora-react-cart/static/js/main.46258824.chunk.js"
+  },
+  {
+    "revision": "2f2b2f33e30a714cafc9",
+    "url": "/reestora-react-cart/static/js/runtime~main.3e547d17.js"
+  },
+  {
+    "revision": "37a9b4201a7917cd0bd61b6ed674ddbb",
+    "url": "/reestora-react-cart/static/media/Item1.37a9b420.jpg"
+  },
+  {
+    "revision": "346557c3ae4f0c21daacb55bccf2a0f6",
+    "url": "/reestora-react-cart/static/media/Item2.346557c3.jpg"
+  },
+  {
+    "revision": "03ac90e240258fa1005fa05114bb8b4f",
+    "url": "/reestora-react-cart/static/media/Item3.03ac90e2.jpg"
+  },
+  {
+    "revision": "96c6f156439830c16aa2ad17d0204514",
+    "url": "/reestora-react-cart/static/media/Item4.96c6f156.jpg"
+  },
+  {
+    "revision": "b62273c8bcd85b6d355c571c48f382a2",
+    "url": "/reestora-react-cart/static/media/Item5.b62273c8.jpg"
+  },
+  {
+    "revision": "03ac90e240258fa1005fa05114bb8b4f",
+    "url": "/reestora-react-cart/static/media/Item6.03ac90e2.jpg"
+  },
+  {
+    "revision": "a90bae4f99fd8f1e2e101d5d49dce04f",
+    "url": "/reestora-react-cart/static/media/header.a90bae4f.jpg"
+  }
+]);
